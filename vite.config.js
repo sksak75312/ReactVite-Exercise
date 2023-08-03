f02ed15ff1cea53b4ca65da58cd3fc7305ead24b
sksak75312 <sksak75312@gmail.com>
2023-08-03 12:57:32 +0800

    設定環境變數

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 開發中、生產中
+  base: process.env.NODE_ENV === "production" ? "/ReactVite-Exercise/" : "/",
   plugins: [react()],
 })
